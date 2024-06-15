c3c5aa34245ae61916387ce4832ef816e627e498
anshika361 <anshikaagarwal631@gmail.com>
2024-06-15 14:09:30 +0530

    add changes

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -4,7 +4,7 @@ import { Community, Logo } from '../assets'
 
 const Login = () => {
     return (
-        <div className='flex flex-col md:grid md:grid-cols-2 min-h-screen bg-gradient-to-r from-sky-950 to-sky-800'>
+        <div className='flex flex-col md:grid md:grid-cols-2 min-h-screen bg-gradient-to-r from-sky-950 to-sky-900'>
             <div className='flex items-center justify-center p-8'>
                 <Form />
             </div>
